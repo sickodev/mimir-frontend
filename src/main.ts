474aa3680e34510a4b5df52273490739ce3e91d4
sickodev <kbishwa03kalyan@gmail.com>
2025-07-08 13:33:47 +0530

    Initial Commit

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import "./style.css"
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <h1 class="">Hello World</h1>
+  </div>
+`
+
---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+    server: {
+        port: 3000
+    },
+    plugins: [
+        tailwindcss()
+    ]
+})
